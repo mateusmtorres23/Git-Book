8e885494a178b997ddc13a2d19742b7754366aa4
Mateus Torres <mmtorres230726@gmail.com>
2026-09-04 10:48:42 -0300

    feat: Initalizing project

---
+++ src/main/java/com/gitbook/gitbook/GitbookApplication.java
@@ -0,0 +1,13 @@
+package com.gitbook.gitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitbook/gitbook/GitbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitbook.gitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
